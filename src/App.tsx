146ba25e479fe8c0e77c0bee2ed8db9e5b0b77d8
TobiaszLos <tobiasz1512@gmail.com>
2023-02-23 23:32:42 +0100

    Dark theme toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import { useDarkMode } from './hooks/useDarkMode'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs'
 
 export const App = () => {
   const [darkTheme, setDarkTheme] = useDarkMode()
 
   return (
     <HelmetProvider>
-      <div>
-        <Helmet>
-          <body className={darkTheme} />
-        </Helmet>
-
-        <button
-          onClick={setDarkTheme}
-          className="rounded-2xl border px-6 py-1 dark:text-white dark:bg-slate-800"
-        >
-          Theme {darkTheme}
-        </button>
-      </div>
+      <Helmet>
+        <body className={darkTheme} />
+      </Helmet>
+      <section className='dark:bg-slate-800 text-white h-screen'>
+        <nav>
+          <div className='flex justify-end p-4'>
+            <input
+              type="checkbox"
+              id="dark-mode"
+              className="peer invisible"
+              checked={darkTheme !== 'dark'}
+              onChange={setDarkTheme}
+            />
+            <label
+              htmlFor="dark-mode"
+              className="flex bg-slate-600 w-12 h-5 rounded-3xl p-1 relative [&>div]:peer-checked:translate-x-7 justify-between items-center cursor-pointer"
+            >
+              <BsFillSunFill className="text-amber-400 scale-75" />
+              <BsFillMoonFill className="text-blue-400 scale-75" />
+              <div className=" w-5 h-5 bg-white flex rounded-full absolute top-0 left-0 mt-0 transition-transform duration-200 ease-linear scale-75"></div>
+            </label>
+          </div>
+        </nav>
+        <section></section>
+      </section>
     </HelmetProvider>
   )
 }
-
-
