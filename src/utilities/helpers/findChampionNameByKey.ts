028a91228537f3d86254c4b227f2526d1772149b
TobiaszLos <tobiaszpost2@gmail.com>
2023-07-07 20:37:11 +0200

    helpers

---
+++ src/utilities/helpers/findChampionNameByKey.ts
@@ -0,0 +1,20 @@
+import { ChampionData } from "../../types"
+
+export const findChampionNameByKey =  (
+  objRecord: Record<string, ChampionData>,
+  keyToFind: string
+) => {
+  // return  await fetchChampionsData()
+
+  const foundChampion = Object.values(objRecord).find(
+    (champion) => champion.key === keyToFind
+  )
+
+  if (foundChampion) {
+    console.log('Znaleziono bohatera:', foundChampion)
+    return foundChampion.id
+  } else {
+    console.log('Nie znaleziono bohatera o kluczu', keyToFind)
+    return undefined
+  }
+}
