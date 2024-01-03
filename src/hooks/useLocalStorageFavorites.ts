f1dadb7c4bbce47f9edf335d76d96cf011ffb4b5
TobiaszLos <tobiaszpost2@gmail.com>
2024-01-03 23:08:46 +0100

    update hook useLocalStorageFavorites

--- src/hooks/useLocalStorageFavorites.ts
+++ src/hooks/useLocalStorageFavorites.ts
@@ -1,32 +1,39 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const useLocalStorageFavorites = (key: string) => {
-  const [favorites, setFavorites] = useState<string[]>(() => {
-    const profilesFromLocalStorage = window.localStorage.getItem(key);
-    return profilesFromLocalStorage ? JSON.parse(profilesFromLocalStorage) : [];
-  });
+  const [favorites, setFavorites] = useState<
+    { name: string; server: string }[]
+  >(() => {
+    const profilesFromLocalStorage = window.localStorage.getItem(key)
+    return profilesFromLocalStorage ? JSON.parse(profilesFromLocalStorage) : []
+  })
 
-  const saveFavoriteToLocalStorage = (newName: string) => {
+  const saveFavoriteToLocalStorage = (newName: string, server: string) => {
     setFavorites((prevFavorites) => {
-      const listOfFavoritesProfiles = [...prevFavorites, newName];
-      window.localStorage.setItem(key, JSON.stringify(listOfFavoritesProfiles));
-      return listOfFavoritesProfiles;
-    });
-  };
+      const listOfFavoritesProfiles = [
+        ...prevFavorites,
+        { name: newName, server: server },
+      ]
+      window.localStorage.setItem(key, JSON.stringify(listOfFavoritesProfiles))
+      return listOfFavoritesProfiles
+    })
+  }
 
   const removeFavoriteFromLocalStorage = (name: string) => {
     setFavorites((prevFavorites) => {
-      const updatedFavorites = prevFavorites.filter((favorite) => favorite !== name);
-      window.localStorage.setItem(key, JSON.stringify(updatedFavorites));
-      return updatedFavorites;
-    });
-  };
+      const updatedFavorites = prevFavorites.filter(
+        (favorite) => favorite.name !== name
+      )
+      window.localStorage.setItem(key, JSON.stringify(updatedFavorites))
+      return updatedFavorites
+    })
+  }
 
   return {
     favorites,
     saveFavoriteToLocalStorage,
     removeFavoriteFromLocalStorage,
-  };
-};
+  }
+}
 
-export default useLocalStorageFavorites;
+export default useLocalStorageFavorites
